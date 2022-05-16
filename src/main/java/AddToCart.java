e0b21922148e6bfcc5b6da99449341dc7da19229
Atique <atique1224@gmail.com>
2022-05-16 12:08:19 +0530

    Updated code

--- src/main/java/AddToCart.java
+++ src/main/java/AddToCart.java
@@ -3,8 +3,6 @@ import java.util.Arrays;
 
 import java.util.List;
 
-import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
 
 import org.openqa.selenium.WebDriver;
--- src/main/java/AmazonInterview.java
+++ src/main/java/AmazonInterview.java
@@ -2,8 +2,6 @@ import java.util.Arrays;
 
 import java.util.List;
 
-import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
 
 import org.openqa.selenium.WebDriver;
@@ -11,10 +9,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import org.openqa.selenium.chrome.ChromeDriver;
-
-import org.openqa.selenium.support.ui.ExpectedConditions;
-
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class AmazonInterview {
 
